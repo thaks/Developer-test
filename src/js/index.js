106498b4355eff12fa29a3d2a856d16ad3cd0f55
tahir <thrmlp@gmail.com>
2021-02-02 01:29:00 +0400

    Pollyfill Setup for new javascript dot methods to support old browsers.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
+
+// Pollyfill Import
+import "core-js/stable";
+import "core-js/modules/es.object.values";
+import "core-js/modules/es.promise";
+import "regenerator-runtime/runtime"
+
+// Sass Import
 import '../scss/main.scss'
+
 
 alert("Hi")
