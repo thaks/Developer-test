53feef4f5f4d633e4311585ce6276c45f2db6865
tahir <thrmlp@gmail.com>
2021-02-02 02:07:59 +0400

    Testing Componet Mount in the Body Element

---
+++ src/js/views/base.js
@@ -0,0 +1,3 @@
+export const elements = {
+    body: document.querySelector('body'),
+}
--- src/js/views/components/index.js
+++ src/js/views/components/index.js
@@ -10,7 +10,8 @@ export const Wrapper = (wrapperClassName, childrens) => {
 }
 
 
-// Header Components //////////////////
+// Header Components 
+//////////////////////////////////////
 
 // Component - LogoBox
 export const LogoBox = (imageSource) => {
@@ -20,4 +21,51 @@ export const LogoBox = (imageSource) => {
     logobox.classList.add('center-horizontal')
     logobox.innerHTML = htmlString;
     return logobox;
+}
+
+// Component - NavLink
+export const NavLink = (linkText, linktype) => {
+    const htmlString = `${linkText}`;
+    const navlink = document.createElement('div')
+    if(linktype) navlink.dataset.type = linktype
+    navlink.classList.add('navlink')
+    navlink.innerHTML = htmlString;
+    return navlink;
+}
+
+// Component - NavBox
+export const NavBox = (childrens) => {
+    const navbox = document.createElement('nav')
+    navbox.classList.add('nav')
+    childrens.map(child => {
+        navbox.append(child)
+    })
+    return navbox;
+}
+
+// Component - SearchInput
+export const SearchInput = () => {
+    const searchInput = document.createElement('div');
+    searchInput.classList.add('search')
+    
+    const searchButton = document.createElement('div');
+    searchButton.classList.add('search__button')
+
+    const input = document.createElement("input");
+    input.classList.add('search__input')
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "Search...");
+
+    var searchIcon = document.createElement("img");
+    searchIcon.setAttribute("src", "https://www.flaticon.com/svg/vstatic/svg/622/622669.svg?token=exp=1612131966~hmac=1b20397265a8d6f495ea0c62bafca1d3");
+    searchIcon.setAttribute("width", "17");
+    searchIcon.setAttribute("height", "17");
+    searchIcon.setAttribute("alt", "Search");
+    searchIcon.classList.add('search__icon')
+
+    searchButton.append(searchIcon);
+    searchInput.append(input);
+    searchInput.append(searchButton);
+    
+    return searchInput;
 }
